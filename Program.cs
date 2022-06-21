2de91756e93450a31ee8d13935ce93ab1218af93
«Тумар <otumar@yandex.ru>
2022-06-22 00:45:51 +0300

    Task 01

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+
+int Prompt(string massage)
+{
+    System.Console.Write(massage);
+    string param = Console.ReadLine();
+    return int.Parse(param);
+}
+int number = Prompt("Введите первое число: ");
+int number2 = Prompt("Введите второе число: ");
+int res = 1;
+for (int i=1; i <= number2; i++) 
+{
+    res = res*number;
+}
+System.Console.WriteLine("Первое число в степени второго числа: "+ res);
